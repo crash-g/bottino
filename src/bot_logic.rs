f8e1f653852ae176b0f7b74ce4666652c1323fdf
crash <crash@inventati.org>
2023-06-24 13:28:15 +0200

    Improve algorithm to compute balance

--- src/bot_logic.rs
+++ src/bot_logic.rs
@@ -27,22 +27,24 @@ use crate::types::{MoneyExchange, SavedExpense};
 /// NP-complete and this approximation is normally good enough.
 ///
 /// Another approximation is that we use floating-point math: this may cause rounding
-/// errors, but in general we tolerate errors up to one cent.
+/// errors, but in general we tolerate errors up to one cent. In the future it may
+/// be better to use fixed-precision numbers, since the integral part is what
+/// we care about and we don't need a lot of precision in the decimal part.
 pub fn compute_exchanges(expenses: Vec<SavedExpense>) -> Vec<MoneyExchange> {
     let debts_and_credits = compute_debts_and_credits(expenses);
     let mut debtors: Vec<_> = debts_and_credits
         .iter()
-        .filter_map(|(p, &a)| if a < 0 { Some((p, a)) } else { None })
+        .filter_map(|(p, &a)| if a < 0.0 { Some((p, a)) } else { None })
         .collect();
     let mut creditors: Vec<_> = debts_and_credits
         .iter()
-        .filter_map(|(p, &a)| if a > 0 { Some((p, a)) } else { None })
+        .filter_map(|(p, &a)| if a > 0.0 { Some((p, a)) } else { None })
         .collect();
 
     if log_enabled!(Debug) {
-        let sum: i64 = debts_and_credits.iter().map(|(_, m)| m).sum();
-        if sum != 0 {
-            debug!("Total sum should be zero. In reality it is {sum}");
+        let sum: i64 = debts_and_credits.iter().map(|(_, m)| m.round() as i64).sum();
+        if sum > 1 || sum < -1 {
+            debug!("Total sum should be 0 (or 1 or -1 in some corner cases). In reality it is {sum}");
             debug!("{:?}", &debtors);
             debug!("{:?}", &creditors);
         }
@@ -56,14 +58,14 @@ pub fn compute_exchanges(expenses: Vec<SavedExpense>) -> Vec<MoneyExchange> {
             .pop()
             .expect("just checked creditors are non-empty!");
         if are_amount_equal(debtor.1, creditor.1) {
-            result.push(MoneyExchange::new(debtor.0, creditor.0, creditor.1));
+            result.push(MoneyExchange::new(debtor.0, creditor.0, creditor.1.round() as i64));
         } else if -debtor.1 < creditor.1 {
             let debt = -debtor.1;
-            result.push(MoneyExchange::new(debtor.0, creditor.0, debt));
+            result.push(MoneyExchange::new(debtor.0, creditor.0, debt.round() as i64));
             creditors.push((creditor.0, creditor.1 - debt));
         } else {
             let debt = creditor.1;
-            result.push(MoneyExchange::new(debtor.0, creditor.0, debt));
+            result.push(MoneyExchange::new(debtor.0, creditor.0, debt.round() as i64));
             debtors.push((debtor.0, debtor.1 + debt));
         }
     }
@@ -85,11 +87,16 @@ pub fn compute_exchanges(expenses: Vec<SavedExpense>) -> Vec<MoneyExchange> {
 
 /// Some debts cannot be split exactly (there are no fractions of a cent),
 /// so we tolerate one cent of error when comparing equality.
-fn are_amount_equal(d: i64, c: i64) -> bool {
-    (d + c).abs() < 1
+fn are_amount_equal(d: f64, c: f64) -> bool {
+    (d + c).abs() < 1.0
 }
 
-fn compute_debts_and_credits(expenses: Vec<SavedExpense>) -> HashMap<String, i64> {
+
+/// In a previous version this was a map of integers, but this meant that each
+/// expense potentially introduced a one cent error and in unfortunate scenarios
+/// these errors summed up. So now we wait until the last second (i.e. when outputting
+/// a money exchange) before converting back to integer.
+fn compute_debts_and_credits(expenses: Vec<SavedExpense>) -> HashMap<String, f64> {
     let mut balance = HashMap::new();
 
     for expense in expenses {
@@ -100,8 +107,8 @@ fn compute_debts_and_credits(expenses: Vec<SavedExpense>) -> HashMap<String, i64
     balance
 }
 
-fn compute_debts(expense: &SavedExpense, balance: &mut HashMap<String, i64>) {
-    let mut total_amount = expense.amount;
+fn compute_debts(expense: &SavedExpense, balance: &mut HashMap<String, f64>) {
+    let mut total_amount = expense.amount as f64;
 
     let fixed_debtors: Vec<_> = expense
         .participants
@@ -123,20 +130,21 @@ fn compute_debts(expense: &SavedExpense, balance: &mut HashMap<String, i64>) {
     let all_others_len = all_others.len();
 
     for p in fixed_debtors {
-        let amount = p.amount.expect("fixed debtors must have a custom amount!");
-        let entry = balance.entry(p.name.clone()).or_insert(0);
+        let amount = p.amount.expect("fixed debtors must have a custom amount!") as f64;
+        let entry = balance.entry(p.name.clone()).or_insert(0.0);
         *entry -= amount;
         total_amount -= amount;
     }
-    let single_quota = (total_amount as f64 / all_others_len as f64).round() as i64;
+
+    let single_quota = total_amount / all_others_len as f64;
     for p in all_others {
-        let entry = balance.entry(p.clone()).or_insert(0);
+        let entry = balance.entry(p.clone()).or_insert(0.0);
         *entry -= single_quota;
     }
 }
 
-fn compute_credits(expense: &SavedExpense, balance: &mut HashMap<String, i64>) {
-    let mut total_amount = expense.amount;
+fn compute_credits(expense: &SavedExpense, balance: &mut HashMap<String, f64>) {
+    let mut total_amount = expense.amount as f64;
 
     let fixed_creditors: Vec<_> = expense
         .participants
@@ -153,20 +161,21 @@ fn compute_credits(expense: &SavedExpense, balance: &mut HashMap<String, i64>) {
     for p in fixed_creditors {
         let amount = p
             .amount
-            .expect("fixed creditors must have a custom amount!");
-        let entry = balance.entry(p.name.clone()).or_insert(0);
+            .expect("fixed creditors must have a custom amount!") as f64;
+        let entry = balance.entry(p.name.clone()).or_insert(0.0);
         *entry += amount;
         total_amount -= amount;
     }
-    let single_quota = (total_amount as f64 / other_creditors_len as f64).round() as i64;
+    let single_quota = total_amount / other_creditors_len as f64;
     for p in other_creditors {
-        let entry = balance.entry(p.name.clone()).or_insert(0);
+        let entry = balance.entry(p.name.clone()).or_insert(0.0);
         *entry += single_quota;
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use approx::assert_abs_diff_eq;
     use chrono::{DateTime, Utc};
 
     use crate::types::SavedParticipant;
@@ -202,9 +211,9 @@ mod tests {
 
         let balance = compute_debts_and_credits(expenses);
 
-        assert_eq!(*balance.get("name1").expect("test"), 590);
-        assert_eq!(*balance.get("name2").expect("test"), 1550);
-        assert_eq!(*balance.get("name3").expect("test"), -2140);
+        assert_abs_diff_eq!(*balance.get("name1").expect("test"), 590.0);
+        assert_abs_diff_eq!(*balance.get("name2").expect("test"), 1550.0);
+        assert_abs_diff_eq!(*balance.get("name3").expect("test"), -2140.0);
     }
 
     #[test]
